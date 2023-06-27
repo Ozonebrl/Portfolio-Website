a4d68bff6966711b535b1db8d6ff65d51479aade
ozonebrl <ozonebrl7@gmail.com>
2023-06-27 09:21:29 +0545

    js changes

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     //variables
-    var words=[" Web Designer","  Digital Marketer"," Developer","FreeLancer","Youtuber"],
+    var words=[" Web Designer","  Digital Marketer"," Developer","FreeLancer","Youtuber","Graphics Designer"],
     part,
     i=0,
     offset=0,
