07fc8f92cb42f561fb88175a15f7b6bd61602c15
ozonebrl <ozonebrl7@gmail.com>
2024-06-19 07:55:39 +0545

    updated js

--- js.js
+++ js.js
@@ -1,53 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
-    //variables
-    var words=["Web Designer","Digital Marketer","Java Developer","Python Developer","FreeLancer","Youtuber","Graphics Designer"],
-    part,
-    i=0,
-    offset=0,
-    len = words.length,
-    forwards=true,
-    skip_count=0,
-    skip_delay=17,
-    speed=50;
+    // Variables
+    var words = ["Web Designer", "Digital Marketer", "Java Developer", "Python Developer", "FreeLancer", "Youtuber", "Graphics Designer"];
+    var part;
+    var i = 0;
+    var offset = 0;
+    var len = words.length;
+    var forwards = true;
+    var skip_count = 0;
+    var skip_delay = 17;
+    var speed = 50;
     
-    const CWAutoTyping=document.querySelector(".codewheel-auto-typing");
+    const CWAutoTyping = document.querySelector(".codewheel-auto-typing");
     
     CWAutoTyping.style.whiteSpace = 'nowrap';
-    function typingtext(){
-        setInterval(function(){
-            if(forwards){
-                if(offset>=words[i].length){
-                    
-                    ++skip_count;432
-                    if(skip_count==skip_delay){
-                        forwards =false;
-                        skip_count=0;
-
+    
+    function typingtext() {
+        setInterval(function() {
+            if (forwards) {
+                if (offset >= words[i].length) {
+                    skip_count++;
+                    if (skip_count == skip_delay) {
+                        forwards = false;
+                        skip_count = 0;
                     }
+                } else {
+                    offset++;
                 }
-
-            }else{
-                if(offset==0){
-                    forwards=true;
+            } else {
+                if (offset == 0) {
+                    forwards = true;
                     i++;
-                    offset=0;
-                    if(i>=len){
-                        i=0;
-
+                    if (i >= len) {
+                        i = 0;
                     }
+                } else {
+                    offset--;
                 }
             }
-            part=words[i].substr(0,offset);
-            if(skip_count==0){
-                if(forwards){
-                    offset++;
-                }else{
-                    offset--;
-
-                }
-                }
-                CWAutoTyping.textContent=part;
-            },speed );
+            part = words[i].substr(0, offset);
+            CWAutoTyping.textContent = part;
+        }, speed);
     }
+    
     typingtext();
 });
